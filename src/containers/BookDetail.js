988c534e9c1ac542b20a7c736b685c37e9b0c35e
Julien LaPointe <email@julienlapointe.com>
2017-08-16 19:21:06 -0500

    cleaned up the BookDetail Container by deleting unnecessary lines

--- src/containers/BookDetail.js
+++ src/containers/BookDetail.js
@@ -1,10 +1,6 @@
 import React, {Component} from "react";
 // add the connect() method from the "react-redux" library to convert the "dumb component" BookDetail into the "smart component" / Container BookDetail by passing in a subset of state as props into BookDetail
 import {connect} from "react-redux";
-// add the selectBook Action Creator 
-import {selectBook} from "../actions/index.js";
-// bindActionCreators ensures the Action (an object with a "type" and "payload" property) returned by the selectBook Action Creator flows through all the Reducers
-import {bindActionCreators} from "redux";
 
 class BookDetail extends Component {
 	render() {
@@ -33,15 +29,6 @@ function mapStateToProps(state) {
 	return {
 		book: state.activeBook
 	};
-}
-
-// whatever is returned by this function will end up as props inside the BookDetail Container
-function mapDispatchToProps(dispatch) {
-	// selectBook is the Action Creator (a simple function that returns an object) that we imported above from actions/index.js
-	// bindActionCreators() ensures that whenver the selectBook Action Creator is called, the returned value (= an Action = an object with a "type" and "payload" property) is passed to all Reducers
-	// dispatch() function is what actually sends the Action to all Reducers
-	// this.props.selectBook() will call the Action Creator
-	return bindActionCreators({selectBook: selectBook}, dispatch)
 }
 
 // the "react-redux" library's connect() method accepts: 
